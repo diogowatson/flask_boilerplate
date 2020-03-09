e2a8b0081889ec86f1d066acf53a139c8ced2f67
Diogo Watson <diogowatson@gmail.com>
2020-03-08 22:26:24 -0400

    Update sqldatabase.py

--- sqldatabase.py
+++ sqldatabase.py
@@ -1,5 +1,4 @@
 from mysql_parameters import MySQLParameters as pr
 
-
 def get_connection_string():
     return 'mysql://:' + pr.username + '/' + pr.password + '/' + pr.db_endpoint + '/' + pr.db_instance
